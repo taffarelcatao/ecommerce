7a091f638bbb894e0aef633b64799ae1467de0e4
unknown <taffarel-182@hotmail.com>
2021-08-05 10:25:29 -0400

    site - produtos

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php 
 
+function formatPrice(float $vlprice){
+
+	return number_format($vlprice, 2, ",", ".");
+
+}
+
 function post($key)
 {
 	return str_replace("'", "", $_POST[$key]);
--- site.php
+++ site.php
@@ -1,12 +1,17 @@
 <?php
 
 use \Hcode\Hcode\Page;
+use \Hcode\Model\Product;
 
 $app->get('/', function() {
+
+	$products = Product::listAll();
     
 	$page = new Hcode\Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		'products'=>Product::checkList($products)
+	]);
 
 });
 
